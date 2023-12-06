82820be403e7ea003331ee20b89d6b7b89506a7b
ch9901 <jcy990113@gmail.com>
2023-12-06 17:39:10 +0900

    231206

---
+++ 12/231206/01react/src/App.js
@@ -0,0 +1,117 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import React, { useReducer, useRef, useEffect, useState } from "react";
+import Diary from "./pages/Diary";
+import Home from "./pages/Home";
+import Edit from "./pages/Edit";
+import New from "./pages/New";
+
+const mockData = [
+  {
+    id: "mock1",
+    date: new Date().getTime() - 1,
+    content: "오늘의 일기",
+    emotionId: 1,
+  },
+  {
+    id: "mock2",
+    date: new Date().getTime() - 2,
+    content: "오늘의 일기",
+    emotionId: 3,
+  },
+  {
+    id: "mock3",
+    date: new Date().getTime() - 3,
+    content: "오늘의 일기",
+    emotionId: 2,
+  },
+];
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "CREATE": {
+      return [action.data, ...state];
+    }
+    case "UPDATE": {
+      return state.map((item) =>
+        String(item.id) === String(action.targetId) ? { ...action.data } : it
+      );
+    }
+
+    case "DELETE": {
+      return state.filter((item) => String(item.id) !== String(item.targetId));
+    }
+    case "INIT": {
+      return mockData;
+    }
+    default:
+      return state;
+  }
+};
+
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
+function App() {
+  const [isDataLoaded, setIsDateLoaded] = useState(false);
+  const [data, dispatch] = useReducer(reducer, []);
+  const idRef = useRef(0);
+  useEffect(() => {
+    dispatch({
+      type: "INIT",
+      data: mockData,
+    });
+    setIsDateLoaded(true);
+  }, []);
+
+  const onCreate = (date, content, emotionId) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current,
+        date: new Date(date).getTime(),
+        content,
+        emotionId,
+      },
+    });
+    idRef.current += 1;
+  };
+
+  const onUpdate = (targetId, date, content, emotionId) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotionId,
+      },
+    });
+  };
+
+  const onDelete = (targetId) => {
+    dispatch({
+      type: "DELETE",
+      targetId,
+    });
+  };
+  if (!isDataLoaded) {
+    return <div>데이터를 불러오는 중 입니다!</div>;
+  } else {
+    return (
+      <DiaryStateContext.Provider value={data}>
+        <DiaryDispatchContext.Provider value={{ onCreate, onDelete, onUpdate }}>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/new" element={<New />} />
+              <Route path="/diary/:id" element={<Diary />} />
+              <Route path="/edit/:id" element={<Edit />} />
+            </Routes>
+          </div>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
+    );
+  }
+}
+
+export default App;
---
+++ 12/231206/01react/src/util.js
@@ -0,0 +1,95 @@
+import emotion1 from "./content/emotion1.png";
+import emotion2 from "./content/emotion2.png";
+import emotion3 from "./content/emotion3.png";
+import emotion4 from "./content/emotion4.png";
+import emotion5 from "./content/emotion5.png";
+
+export const getEmotionById = (emotionId) => {
+  const targetEmotionId = String(emotionId);
+  switch (targetEmotionId) {
+    case "1":
+      return emotion1;
+    case "2":
+      return emotion2;
+    case "3":
+      return emotion3;
+    case "4":
+      return emotion4;
+    case "5":
+      return emotion5;
+    default:
+      return null;
+  }
+};
+
+//emotion img refactoring
+export const emotionList = [
+  {
+    id: 1,
+    name: "최고",
+    img: getEmotionById(1),
+  },
+  {
+    id: 2,
+    name: "좋음",
+    img: getEmotionById(2),
+  },
+  {
+    id: 3,
+    name: "보통",
+    img: getEmotionById(3),
+  },
+  {
+    id: 4,
+    name: "별로",
+    img: getEmotionById(4),
+  },
+  {
+    id: 5,
+    name: "최악",
+    img: getEmotionById(5),
+  },
+];
+
+//새로고침하면 초기값 설정
+export const getFormattedDate = (targetDate) => {
+  let year = targetDate.getFullYear();
+  let month = targetDate.getMonth() + 1;
+  let date = targetDate.getDate();
+
+  if (month < 10) {
+    month = `0${month}`;
+  }
+
+  if (date < 10) {
+    date = `0${date}`;
+  }
+  return `${year}-${month}-${date}`;
+};
+
+export const getMonthRangeByDate = (date) => {
+  const todayYear = date.getFullYear();
+  const todayMonth = date.getMonth();
+  const beginTimeStamp = new Date(todayYear, todayMonth, 1, 0, 0, 0).getTime();
+  const endTimeStamp = new Date(
+    todayYear,
+    todayMonth + 1,
+    0,
+    23,
+    59,
+    59
+  ).getTime();
+  return { beginTimeStamp, endTimeStamp };
+};
+
+
+export const sortOptionList = [
+  {
+    value: "latest",
+    name: "최신 순",
+  },
+  {
+    value: "oldest",
+    name: "오래된 순",
+  },
+];
